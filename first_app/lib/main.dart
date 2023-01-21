15a757c271bf27bf8f7f37e9c4fc6cb1ad934950
Srivastava-Ankit-LPU <ankshrivastava17@gmail.com>
2023-01-21 23:42:05 +0530

    initialised the first app

---
+++ first_app/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main(){
+
+runApp(MaterialApp(home: Scaffold(
+  body: Center(child: Text("Hello WOrld")),
+  appBar: AppBar(title: Center(child: Text("My App Bar"))),
+  )
+  )
+  );
+//android->material theme
+
+//ios->cupurtino theme
+}
